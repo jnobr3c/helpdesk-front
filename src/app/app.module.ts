0a151ec8065b9f4a44a81c7c2509c596daa19bf1
unknown <jnobr3c+github@gmail.com>
2026-03-22 11:34:02 -0300

    :sparkles: feat: interceptor para requisições http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
 import { ToastrModule } from 'ngx-toastr';
+import { authInterceptorProviders } from './interceptor/auth.interceptor';
 
 
 @NgModule({
@@ -77,7 +78,7 @@ import { ToastrModule } from 'ngx-toastr';
       progressBar: true
 })
   ],
-  providers: [],
+  providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
